9b149aded1ffb97269fd1af65970b323b0ddd88b
SiddhiKhunt01 <167965206+SiddhiKhunt01@users.noreply.github.com>
2024-12-10 17:07:40 +0530

    Add files via upload

---
+++ Projetc_5-Siddhi/src/components/Navbar.jsx
@@ -0,0 +1,116 @@
+import dropdown from "../assets/drop.svg";
+import List from "../assets/list.svg";
+import phone from "../assets/phone.svg";
+
+const navbar = () => {
+  return (
+    <>
+      <div className="custom-container">
+        <nav class="navbar navbar-height navbar-expand-lg border-0 d-flex justify-content-between align-items-center p-0">
+          <div>
+            <img src={List} alt="list" className="list-logo"/>
+          </div>
+          <div id="navbarNavDropdown">
+            <ul class="navbar-nav main-navbar">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="dropdown-button nav-link ">
+                    Category
+                    <img src={dropdown} className="dropdown-logo" alt="dropdown" />
+                  </button>
+                  <div class="dropdown-content">
+                    <a href="#">Shop Left Sidebar</a>
+                    <a href="#">Shop Right Sidebar</a>
+                    <a href="#">Full Width</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="dropdown-button nav-link">
+                    Products
+                    <img src={dropdown} className="dropdown-logo" alt="dropdown" />
+                  </button>
+                  <div class="dropdown-content">
+                    <a href="#">Product Left Sidebar</a>
+                    <a href="#">Product Right Sidebar</a>
+                    <a href="#">Product Full Width</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="dropdown-button nav-link">
+                    Pages
+                    <img src={dropdown} className="dropdown-logo" alt="dropdown" />
+                  </button>
+                  <div class="dropdown-content">
+                    <a href="#">About Us</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Cart</a>
+                    <a href="#">Checkout</a>
+                    <a href="#">Track Order</a>
+                    <a href="#">Wishlist</a>
+                    <a href="#">FAQ</a>
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                    <a href="#">Policy</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="dropdown-button nav-link">
+                    Blog <img src={dropdown} className="dropdown-logo" alt="dropdown" />
+                  </button>
+                  <div class="dropdown-content">
+                    <a href="#">Left Sidebar</a>
+                    <a href="#">Right Sidebar</a>
+                    <a href="#">Full Width</a>
+                    <a href="#">Detail Left Sidebar</a>
+                    <a href="#">Detail Right Sidebar</a>
+                    <a href="#">Detail Full Width</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="dropdown-button nav-link">
+                    Elements
+                    <img src={dropdown} className="dropdown-logo" alt="dropdown" />
+                  </button>
+                  <div class="dropdown-content">
+                    <a href="#">Products</a>
+                    <a href="#">Typography</a>
+                    <a href="#">Buttons</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p className="phone"><img src={phone} className="phone-logo" alt="phone" />
+            +91 ( 234 ) ( 4563 )</p>
+          </div>
+        </nav>
+      </div>
+      <div className="navigation-background">
+            <div className="custom-container d-flex justify-content-between align-items-center h-100">
+                <div className="page-name">
+                    <h5>Product</h5>
+                </div>
+                <div className="navigation">
+                    Home <span>- Product</span>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default navbar;
